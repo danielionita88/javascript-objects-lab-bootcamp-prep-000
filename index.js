d4342ed07db1920b8dc08fff449d7f8a995ecef0
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-24 18:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.assign({}, obj,{[key]:value})
+ var newObj = object.assign({}, obj,{[key]:value});
+ return newObj;
 }
